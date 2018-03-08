7ad4a923c192d8abecd7324ecd552253f209caaf
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-08 07:38:16 +0100

    name and assign to multiple columns of UNIQUE constraint

--- unique_constraints.sql
+++ unique_constraints.sql
@@ -6,3 +6,8 @@ CREATE TABLE Persons(
 --To create a UNIQUE constraint on the "ID" column when the table is already created, use the following SQL:
 ALTER TABLE persones 
 ADD UNIQUE(ID);
+
+--To name a UNIQUE constraint, and to define a UNIQUE constraint on multiple columns, use the following SQL syntax:
+ALTER TABLE Persons
+ADD CONSTRAINT UC_Persons UNIQUE(ID, LastName);
+
