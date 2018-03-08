b695507f58680e7acbed8557f44d28e141fd20e5
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-08 06:52:56 +0100

    alter table by ALTER TABLE table_name ADD COLUMN column_name data_type

---
+++ alter_table.sql
@@ -0,0 +1,4 @@
+--alter table is used to add, delete and modify the columns in an existing table
+--alter table is used to add and trop various constraints 
+ALTER TABLE Persons
+ADD COLUMN DateOfBirth DATE;
