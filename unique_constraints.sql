70fa3514f91469dc0d2006ea9050613153f9042a
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-08 07:33:43 +0100

    add UNIQUE constraint to existing column

--- unique_constraints.sql
+++ unique_constraints.sql
@@ -1,4 +1,8 @@
-CREATE Persons(
+CREATE TABLE Persons(
     ID int NOT NULL UNIQUE,
     LastName varchar NOT NULL
 );
+
+--To create a UNIQUE constraint on the "ID" column when the table is already created, use the following SQL:
+ALTER TABLE persones 
+ADD UNIQUE(ID);
