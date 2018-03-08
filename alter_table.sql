c5680e130a4c831c8feebdb9eee0fa2de77bbd87
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-08 06:54:32 +0100

    alter column data type by ALTER TABLE table_name ALTER COLUMN column_name data_type

--- alter_table.sql
+++ alter_table.sql
@@ -2,3 +2,7 @@
 --alter table is used to add and trop various constraints 
 ALTER TABLE Persons
 ADD COLUMN DateOfBirth DATE;
+
+--change data type
+ALTER TABLE Persons
+ALTER COLUMN DateOfBirth YEAR;
