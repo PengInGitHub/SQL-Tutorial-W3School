049692488a900dadb56e16736336d0a809348b17
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-08 07:10:48 +0100

    add definition of constraint

--- constraints.sql
+++ constraints.sql
@@ -1,6 +1,7 @@
-/*CREATE table1 (
-    column1 datatype constraint,
-    column2 datatype constraint,
-    ...
-)
+/*Constraints are used to limit the type of data that can go into a table. 
+    CREATE table1 (
+        column1 datatype constraint,
+        column2 datatype constraint,
+        ...
+    )
 */
