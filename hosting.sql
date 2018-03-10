24ede1706d5dc1126f973eb2130d650d43614236
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-10 18:05:22 +0100

    add hosting

---
+++ hosting.sql
@@ -0,0 +1,6 @@
+MySQL
+MySQL is also a popular database software for web sites.
+
+MySQL is a very powerful, robust and full featured SQL database system.
+
+MySQL is an inexpensive alternative to the expensive Microsoft and Oracle solutions.
