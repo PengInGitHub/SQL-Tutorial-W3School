d354f0bbbdedbb9349686a702c37898bc27449a3
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-10 16:36:12 +0100

    use VIEW example 2

--- view.sql
+++ view.sql
@@ -4,3 +4,11 @@ FROM Products
 WHERE Discotiuned = No
 
 SELECT * FROM [Current Product List];
+
+--Products Above Average Price
+CREATE VIEW [Products Above Average Price] AS
+SELECT * 
+FROM Products
+WHERE UnitPrice > (SELECT AVG(UnitPrice) FROM Products)
+
+SELECT * FROM [Products Above Average Price];
