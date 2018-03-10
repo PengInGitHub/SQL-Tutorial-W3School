02fef5290e841d0ebdf6c6b2b931a58893a2e442
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-10 17:52:22 +0100

    add injection example deleting table

--- injection.sql
+++ injection.sql
@@ -10,3 +10,10 @@ SELECT * FROM Users WHERE NAME = "John" AND PASS = "123456";
 --hacker action
 --input uName =  "or""=" , uPass =  "or""=" 
 SELECT * FROM Users WHERE NAME = ""or""="" AND PASS = ""or""=""
+
+
+--hacker action drop table
+txtUserID = getRequestString("UserId")
+txtSQL = "SELECT * FROM Users WHERE UserID = " + txtUserID;
+--input: 105;DROP TABLE Supperliers;
+SELECT * FROM Users WHERE UserID = 105;DROP TABLE Suppliers;
