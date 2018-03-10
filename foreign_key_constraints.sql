7ae5db876dc4075f268bfb7a4ba96131869920c0
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-10 08:29:05 +0100

    naming a FOREIGN KEY constraint

---
+++ foreign_key_constraints.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Orders(
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (PersonID) REFERENCES Persons (PersonID)
+)
+
+--naming foreign key
+CREATE TABLE Orders(
+    OrderID int NOT NULL,
+    OrderNumber int,
+    PersonID int,
+    PRIMARY KEY(OrderID),
+    CONSTRAINT FK_PersonOrder FOREIGN KEY (PersonID) REFERENCES Persons (PersonID)
+)
