3e5791cda6f030637d2752b6a967a4fe9b4305d4
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-10 17:38:55 +0100

    add injection example in username and password

--- injection.sql
+++ injection.sql
@@ -6,3 +6,7 @@ sql = 'SELECT * FROM Users WHERE NAME = "' + uName + '" AND PASS = "' + uPass '"
 --Result:
 --input is uName = John, uPass = 123456 
 SELECT * FROM Users WHERE NAME = "John" AND PASS = "123456";
+
+--hacker action
+--input uName =  "or""=" , uPass =  "or""=" 
+SELECT * FROM Users WHERE NAME = ""or""="" AND PASS = ""or""=""
