a2ceeb1e5fb145d809531e12ebea8e2378b7d379
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-10 15:53:43 +0100

    drop a default constraint

--- default_constraints.sql
+++ default_constraints.sql
@@ -15,4 +15,8 @@ CREATE TABLE Persons(
 
 --set default for existing table
 ALTER TABLE
-ALTER City SET DEFAULT 'Los Angeles' 
+ALTER City SET DEFAULT 'Los Angeles'
+
+--drop a default constraint
+ALTER TABLE
+ALTER City DROP DEFAULT
