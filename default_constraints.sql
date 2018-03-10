2dd7285ee39bfba4fbda4383e689cc32453df0dc
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-10 09:13:35 +0100

    use bulit-in functions for DEFAULT

--- default_constraints.sql
+++ default_constraints.sql
@@ -6,3 +6,10 @@ CREATE TABLE Persons(
     City varchar DEFAULT 'Paris'
 )
 
+--use bulit-in functions for DEFAULT
+CREATE TABLE Persons(
+    ID int NOT NULL,
+    Age int,
+    RegisterDate date DEFAULT GETDATE()
+)
+
